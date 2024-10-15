81a04e140601ff15b3fba3250889c8fe62a9dbad
unknown <bohdankoval3012@gmail.com>
2024-10-15 22:25:11 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-white w-4/5 mx-auto h-screen rounded-xl shadow-xl mt-14">
+    
+    <my-header></my-header>
+
+    <div class=""></div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyHeader from './components/MyHeader.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { MyHeader },};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+            <div class="flex items-center gap-4">
+                <img src="/logo.png" alt="Logo" class="w-10 mr-5" />
+                <div class="">
+                    <h2 class="text-xl font-bold uppercase">Veu Sneakers</h2>
+                    <p class="text-slate-400">Маган кращих кросівок</p>
+                </div>
+            </div>
+
+            <ul class="flex items-center gap-10">
+                <li class="flex items-center cursor-pointer gap-10 text-gray-500 hover:text-black">
+                    <img src="/cart.svg" alt="card" />
+                    <b>1205 грн.</b>
+                </li>
+                <li class="flex items-center cursor-pointer gap-10 text-gray-500 hover:text-black">
+                    <img src="/heart.svg" alt="card" />
+                    <span>Закладки</span>
+                </li>
+                <li class="flex items-center cursor-pointer gap-10 text-gray-500 hover:text-black">
+                    <img src="/profile.svg" alt="card" />
+                    <span>Профіль</span>
+                </li>
+            </ul>
+        </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
